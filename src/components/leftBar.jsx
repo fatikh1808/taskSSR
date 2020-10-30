0dda5f20a5037c81c8c327a0c5ab2dd29016e9be
fatikh1808 <mr.fotih@mail.ru>
2020-10-30 05:18:49 +0300

    Поиграл со стильями, вроде норм responsive получилось

--- src/components/leftBar.jsx
+++ src/components/leftBar.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql, StaticQuery } from "gatsby"
 import "antd/dist/antd.css"
 import { Layout, Menu, Col } from "antd"
@@ -6,6 +6,9 @@ import { Layout, Menu, Col } from "antd"
 const { Sider } = Layout
 
 export const LeftBar = () => {
+
+  const [collapse, setCollapse] = useState(true)
+
 
   return (
     <Col xs={24} sm={6} md={6} lg={3} xl={2} style={{padding: 0}}>
@@ -25,13 +28,15 @@ export const LeftBar = () => {
           <Sider
             breakpoint="lg"
             collapsedWidth="0"
-            width={"100%"}
+            width={collapse ? "100%" : "85%"}
+            onCollapse={(collapsed, type) => {
+              setCollapse(collapsed)
+            }}
             style={{height: "100%"}}
           >
-            <div className="logo"/>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
               {rubrics.map(item => (
-                <Menu.Item key={item.id}>
+                <Menu.Item key={item.id} style={{ height: collapse ? "2px" : "auto"}}>
                   <a href={`/rubric/${item.id}`}>
                     {item.type_name}
                   </a>
